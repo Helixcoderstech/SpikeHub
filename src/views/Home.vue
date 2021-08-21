c3f369624c48b1aa0342941f7334ac29aaa753c7
Cysteine <gbengapeace2001@gmail.com>
2021-08-21 11:03:57 +0100

    new

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,80 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <!--Navbar-->
+    <div class="navbar">
+       <div class="container flex">
+          <h1 class="logo">QUESTHUB </h1>
+            <nav>
+              <ul>
+                <li><a href="Home.html">Home</a></li>
+                  <li><a href="#">Contact</a></li>
+                    <li><a href="#">About</a></li> 
+                      </ul>
+                        </nav>
+                          </div>
+                            </div>
+
+    <!--Showcase-->
+    <section class="showcase">
+      <div class="container grid">
+        <div class="showcase-text">
+          <h1>SPIKE</h1>
+            <p>Welcome to the best platform for online educational institute</p>
+              <a href="#" class="btn btn-outline">Read More</a>
+                </div>
+
+                  <!--div class="card">
+                    </div-->
+                      </div>
+                        </section>
+   
+    <!--Stats-->
+
+    <section class="stats">
+      <div class="container">
+        <h3 class="stats-heading text-center my-1">
+          QuestHub
+            </h3>
+
+              <div class="grid-1">
+                <div class="card">
+                  <img src="science.jpg" alt="">
+                    <a href="Science_Blog.html">
+                     <h3>SCHOOL</h3>
+                       </a>
+                         </div>
+            
+                           <div class="card ">
+                             <img src="campus.jpg" alt=""><a href="Campus_gist.html">
+                               <h3>PAST QUESTIONS</h3>
+                                 </a>
+                                   </div>
+
+                                     <div class="card">
+                                       <img src="entertain.jpg" alt=""><a href="Entertainment.html">
+                                         <h3>QUIZ</h3>
+                                           </a>
+                                             </div>
+                                               </div>
+                                                 </div>
+                                                   </section>
+
+    <!--Footer-->
+        <footer class="footer bg-dark py-5">
+          <div class="container grid grid-3">
+            <div>
+              <h1>HelixCoders</h1>
+                <p>Copyright &copy; 2021</p>
+                  </div>
+                   </div>
+                     </footer>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
